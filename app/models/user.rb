4b787f0f2104bcc5103d358f48409cbdfd6dc9e6
tatsuya0331 <tatsuyuki0215@ezweb.ne.jp>
2021-05-09 21:38:03 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  validates :name,presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          has_many :purchases
          has_many :items
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
   validates :password, presence: true, length: { minimum:6}
   validates :last_name, presence: true
   validates :first_name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2021_04_22_113549) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
