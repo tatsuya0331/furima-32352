3ffb7fa00571146b41b16388913f493de0dceb04
tatsuya0331 <tatsuyuki0215@ezweb.ne.jp>
2021-05-09 16:31:46 +0900

    不要な記述を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
-class ApplicationController < ActionController::Base
-<<<<<<< Updated upstream
-    before_action :authenticate_user!
+class ApplicationController < ActionController::Base    
+  # before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
   
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
-=======
     before_action :configure_permitted_parameters, if: :devise_controller?
 
 
@@ -15,5 +13,4 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
     end
->>>>>>> Stashed changes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-=======
 
   root to: 'homes#index'
   resources :homes do
     resources :homes, only: [:index, :create]
 
   end
->>>>>>> Stashed changes
 end
