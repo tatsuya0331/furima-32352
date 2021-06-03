92f0362d50c9d037beb95319235b5db821051b3b
tatsuya0331 <tatsuyuki0215@ezweb.ne.jp>
2021-06-03 12:46:27 +0900

    コントローラーとfactoriesの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-    before_action :authenticate_user!, only: [:create]
+    before_action :authenticate_user!, only: [:new, :create]
     def index
         
     end
